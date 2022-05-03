c84dc78293c4ce294e09a243c19c087c983df708
Edson Dantas Junior <edsonmdantas@yahoo.com.br>
2022-05-03 15:07:55 -0300

    changes made from edx course

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+
+
+function compute() {
+    var principal = document.getElementById("principal").value;
+    checkData();
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal*years*rate/100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("amount1").innerText = "If you deposit:" + principal;
+    document.getElementById("amount2").innerText = "at an interest rate of:" + rate + "%";
+    document.getElementById("amount3").innerText = "You will receive an amount of:" + interest;
+    document.getElementById("amount4").innerHTML = "in the year:" + year;
 }
-        
+ 
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval + "%";
+
+}
+
+function checkData(){
+    var principal = document.getElementById("principal").value;
+    if (principal=="" || principal<0){
+        alert("Enter a positive number!");
+        principal.focus();
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+function myAlert(){
+    alert("Function Fired!");
+}
